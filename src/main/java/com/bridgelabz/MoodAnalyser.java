16cb7b32f20a12926ebe386262b04d2e6a095193
SUDIPP22 <panja370@gmail.com>
2021-09-15 22:32:59 +0530

    Modified : Respond only Sad Message

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -10,4 +10,13 @@ public class MoodAnalyser {
     public static void main(String[] args) {
         System.out.println("Welcome To The Program");
     }
+
+    /**
+     * This method is created for implementing test cases
+     * @param message This is the first parameter used to store the message
+     * @return returns SAD message
+     */
+    public String analyseMood(String message) {
+            return "SAD";
+    }
 }
---
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MoodAnalyserTest {
+    @Test
+    void givenMessage_WhenSad_ShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a Sad Message");
+        Assertions.assertEquals("SAD", mood);
+    }
+}
